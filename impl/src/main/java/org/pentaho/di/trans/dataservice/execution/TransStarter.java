adf9d6a1760e5adfcf24fa1117bb4c2de9959ab9
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2018-12-29 22:44:19 -0800

    Revert "Set display for trans to execute disposeEmbeddedMetastoreProvider() in the context of the UI session"
    This reverts commit 0e1dd266899649210c281036d83553078568bd83.

--- impl/src/main/java/org/pentaho/di/trans/dataservice/execution/TransStarter.java
+++ impl/src/main/java/org/pentaho/di/trans/dataservice/execution/TransStarter.java
@@ -23,8 +23,6 @@
 package org.pentaho.di.trans.dataservice.execution;
 
 import com.google.common.base.Throwables;
-
-import org.eclipse.swt.widgets.Display;
 import org.pentaho.di.core.exception.KettleException;
 import org.pentaho.di.trans.Trans;
 
@@ -44,7 +42,6 @@ public class TransStarter implements Runnable {
 
   @Override public void run() {
     try {
-      trans.setDisplay( Display.getCurrent() );
       trans.startThreads();
     } catch ( KettleException e ) {
       throw Throwables.propagate( e );
